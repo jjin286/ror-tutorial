347d85d31af7c8f216136ce838616cbfa3989f05
JaeWon Jin <j1jin286@gmail.com>
2025-03-28 09:43:10 -0700

    In Stock Email notification

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "products#index"
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
+  resource :unsubscribe, only: [ :show ]
+
 
 end
